654679afe1a020d4e55e3f147ccc9fe568ee1661
Andrii Zakharchuk <zakharchuk.external@crxmarkets.com>
2023-10-19 08:59:52 +0300

    initial commit

---
+++ src/main/java/com/dev/system/JavaJungleApplication.java
@@ -0,0 +1,12 @@
+package com.dev.system;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+class JavaJungleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJungleApplication.class, args);
+    }
+}
